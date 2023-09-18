6a99eb4d6233e885439946a1c5df52c179b38f47
muqtar <muqtarabdiaziz8@gmail.com>
2023-09-18 11:58:35 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+ 
+  let ride;
+  if(feet<=400){
+    ride="This one is on me!"
+    
+  }
+  else if(feet>2000 && feet <=2500){
+    ride="I will gladly take your thirty bucks."
+  }
+  else{
+    ride="No can do."
+  }
+  return ride;
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+ 
+  return city==="NYC"? "Ok, sounds good.":"No go."
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  let message;
+  if (tip==="generous"){
+message="Thank you so much."
+  }
+  else if(tip==="not as generous"){
+    message="Thank you."
+  }
+  else{
+    message="Bye."
+  }
+  return message;
   // Write your code here!
 }
